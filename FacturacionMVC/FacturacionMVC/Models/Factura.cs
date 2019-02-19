588b0fe291e40b4e6b36800f9acaab5093bd872f
cwlemus <cwlemus@gmail.com>
2019-02-18 20:20:33 -0600

    add NIT
    agregar nit

--- FacturacionMVC/FacturacionMVC/Models/Factura.cs
+++ FacturacionMVC/FacturacionMVC/Models/Factura.cs
@@ -9,6 +9,7 @@ namespace FacturacionMVC.Models
     {
         public int IdFactura { get; set; }
         public int IdCliente { get; set; }     
+        public string Nit { get; set; }     
         public decimal MONTO { get; set;}       
     }
 }
