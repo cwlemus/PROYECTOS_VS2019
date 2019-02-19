095788b9a77921e4e2f3999c0bedbdf0bfab7766
cwlemus <cwlemus@gmail.com>
2019-02-18 20:53:00 -0600

    update Factura.cs/Add desc
    Agregamos descuento

--- FacturacionMVC/FacturacionMVC/Models/Factura.cs
+++ FacturacionMVC/FacturacionMVC/Models/Factura.cs
@@ -8,13 +8,14 @@ namespace FacturacionMVC.Models
     public class Factura
     {
         public int IdFactura { get; set; }
-        public int IdCliente { get; set; }     
+        public int IdCliente { get; set; }           
         public string Nit { get; set; }     
         public decimal MONTO { get; set;}       
 
         public decimal CalcularMonto(decimal subtotal)
         {
-            var total = subtotal * ((decimal)subtotal);
+            var desc= subtotal * (decimal)0.15;
+            var total =( subtotal * ((decimal)subtotal))-desc;
             return total;
         }
     }
